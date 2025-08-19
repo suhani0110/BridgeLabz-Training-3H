fc6c5c620e6904504d38d8fd75b390098c9b3c80
suhani0110 <suhanitomar10@gmail.com>
2025-08-19 17:05:53 +0530

    Create RocketLaunch.java

---
+++ Control Flows/Level 1/RocketLaunch.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RocketLaunch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter countdown start value: ");
+        int counter = sc.nextInt();
+
+        while (counter >= 1) {
+            System.out.println(counter);
+            counter--; // decrement
+        }
+        System.out.println("Liftoff! 🚀");
+        sc.close();
+    }
+}
