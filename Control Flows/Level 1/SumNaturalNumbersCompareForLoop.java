29c8429eed56038877098d638220f241ae7b10ff
suhani0110 <suhanitomar10@gmail.com>
2025-08-19 17:18:44 +0530

    Create SumNaturalNumbersCompareForLoop.java

---
+++ Control Flows/Level 1/SumNaturalNumbersCompareForLoop.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SumNaturalNumbersCompareForLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a natural number: ");
+        int n = sc.nextInt();
+        if (n >= 1) {
+           
+            int formulaSum = n * (n + 1) / 2;
+
+            
+            int loopSum = 0;
+            for (int i = 1; i <= n; i++) {
+                loopSum += i;
+            }
+
+            System.out.println("Sum using formula: " + formulaSum);
+            System.out.println("Sum using for loop: " + loopSum);
+
+            if (formulaSum == loopSum) {
