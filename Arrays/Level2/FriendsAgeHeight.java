e12ad3f24a2ea46cb27a492df354b209430c1e0d
suhani0110 <suhanitomar10@gmail.com>
2025-08-21 23:40:55 +0530

    Added new file

---
+++ Arrays/Level2/FriendsAgeHeight.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class FriendsAgeHeight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        int[] heights = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Enter details for " + names[i] + ":");
+            System.out.print("Age: ");
+            ages[i] = sc.nextInt();
+            System.out.print("Height (in cm): ");
+            heights[i] = sc.nextInt();
+        }
+
+        int youngestIndex = 0;
+        int tallestIndex = 0;
+
+        for (int i = 1; i < 3; i++) {
+            if (ages[i] < ages[youngestIndex]) {
+                youngestIndex = i;
+            }
+            if (heights[i] > heights[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+
+        System.out.println("\n--- Result ---");
+        System.out.println("Youngest: " + names[youngestIndex] + " (Age: " + ages[youngestIndex] + ")");
+        System.out.println("Tallest: " + names[tallestIndex] + " (Height: " + heights[tallestIndex] + " cm)");
+    }
+}
