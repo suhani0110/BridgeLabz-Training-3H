123f54718dacc5ec4a1322bd43a90839c47c6880
suhani0110 <suhanitomar10@gmail.com>
2025-08-27 16:57:52 +0530

    feat: add program of atm transaction

---
+++ ATMtransaction.java
@@ -0,0 +1,34 @@
+
+//package 27-08-2025;
+import java.util.Scanner;
+
+public class ATMtransaction {
+  public static void main(String[] args) {
+    git@SuppressWarnings("resource")
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+    int ans = calculateBalance(arr);
+    System.out.println(ans);
+    if (ans < 0) {
+      System.out.println("Overdraft");
+    }
+  }
+
+  public static int calculateBalance(int[] arr) {
+    int balance = 0;
+    for (int i = 0; i < arr.length; i++) {
+      int t = arr[i];
+      if (t > 0) {
+        balance = balance + t;
+      } else {
+        balance = balance + t;
+      }
+    }
+    return balance;
+  }
+
+}
